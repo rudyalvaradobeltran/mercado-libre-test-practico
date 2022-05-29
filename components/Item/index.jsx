871cbd838561192f144969d6eeca3fdc7b0c512e
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-05-29 16:28:26 -0400

    feat(item by id component): item by id basic info

--- components/Item/index.jsx
+++ components/Item/index.jsx
@@ -2,29 +2,61 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import { itemById } from "../../redux/Item/ItemByIdSlice";
+import Image from "next/image";
+import Grid from "@mui/material/Grid";
+import Button from '@mui/material/Button';
+import { StyledItemDataGrid, StyledItemDescriptionGrid } from "./styles";
 
 const Item = () => {
   const router = useRouter();
   const { id } = router.query;
-  
+
   const dispatch = useDispatch();
   const itemByIdResult = useSelector((store) => store.itemById);
 
   useEffect(() => {
-    if (id && !itemByIdResult.loading && !itemByIdResult.error && !('item' in itemByIdResult.data)) {
+    if (
+      id &&
+      !itemByIdResult.loading &&
+      !itemByIdResult.error &&
+      !("item" in itemByIdResult.data)
+    ) {
       dispatch(itemById(id));
     }
   }, [dispatch, id, itemByIdResult]);
 
   return (
     <>
-        {'item' in itemByIdResult.data &&
-            <div>
-                {itemByIdResult.data.item.title}
-            </div>
-        }
+      {"item" in itemByIdResult.data && (
+        <>
+          <StyledItemDataGrid container spacing={2}>
+            <Grid item xs={3}>
+              <Image
+                src={itemByIdResult.data.item.picture}
+                alt={itemByIdResult.data.item.title}
+                width="500"
+                height="500"
+              />
+            </Grid>
+            <Grid>
+              <p>{`${itemByIdResult.data.item.condition} ${itemByIdResult.data.item.sold_quantity}`}</p>
+              <p>{itemByIdResult.data.item.title}</p>
+              <p>{itemByIdResult.data.item.price.amount}</p>
+              <div>
+                <Button variant="contained">Comprar</Button>
+              </div>
+            </Grid>
+          </StyledItemDataGrid>
+          <StyledItemDescriptionGrid>
+            <Grid item xs={12}>
+              <p>Descripción del producto</p>
+              <p>{itemByIdResult.data.item.description}</p>
+            </Grid>
+          </StyledItemDescriptionGrid>
+        </>
+      )}
     </>
   );
-}
+};
 
 export default Item;
---
+++ components/Item/styles.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+
+export const StyledItemDataGrid = styled(Grid)({
+  
+});
+
+export const StyledItemDescriptionGrid = styled(Grid)({
+  
+});
+
+export default {
+    StyledItemDataGrid,
+    StyledItemDescriptionGrid
+};
