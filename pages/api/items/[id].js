815163f0d44dfaeaf26508769588f1e9307ec4f0
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-06-01 18:48:06 -0400

    feat(api items): redis cache implementation

--- pages/api/items/[id].js
+++ pages/api/items/[id].js
@@ -1,49 +1,70 @@
+const { client } = require("../../../redis/index.js");
+
 export default async function handler(req, res) {
   try {
-    const result = await fetch(
-      `https://api.mercadolibre.com/items/${req.query.id}`
-    ).then((result) => result.json());
+    var result = await client.get(req.query.id, (err, result) => {
+      if (err) {
+        return null;
+      }
+      return result;
+    });
 
-    if (result.error) {
-      res.status(404).send('Not found');
+    if (result) {
+      res.status(200).json(JSON.parse(result));
       return;
+    } else {
+      result = await fetch(
+        `https://api.mercadolibre.com/items/${req.query.id}`
+      ).then((result) => result.json());
+
+      if (result.error) {
+        res.status(404).send("Not found");
+        return;
+      }
+
+      const description = await fetch(
+        `https://api.mercadolibre.com/items/${req.query.id}/description`
+      ).then((result) => result.json());
+
+      const categories = await fetch(
+        `https://api.mercadolibre.com/sites/MLA/search?q=${result["title"]}&limit=1`
+      ).then((result) => result.json());
+
+      const response = { author: { name: "Rudy", lastname: "Alvarado" } };
+
+      response.categories =
+        categories["filters"].length > 0
+          ? categories["filters"][0]["values"][0]["path_from_root"].map(
+              (category) => category.name
+            )
+          : [];
+
+      if (result["id"]) {
+        response.item = {
+          id: result["id"],
+          title: result["title"],
+          price: {
+            amount: result["price"].toString().split(".")[0],
+            currency: result["currency_id"],
+            decimals: result["price"].toString().split(".")[1],
+          },
+          picture: result["pictures"][0]["url"],
+          condition: result["condition"],
+          free_shipping: result["shipping"]["free_shipping"],
+          sold_quantity: result["sold_quantity"],
+          description: description["plain_text"],
+        };
+      }
+
+      await client.set(
+        req.query.id,
+        JSON.stringify(response),
+        "EX",
+        parseInt(process.env.NEXT_PUBLIC_EXPIRES_AT)
+      );
+
+      res.status(200).json(response);
     }
-
-    const description = await fetch(
-      `https://api.mercadolibre.com/items/${req.query.id}/description`
-    ).then((result) => result.json());
-
-    const categories = await fetch(
-      `https://api.mercadolibre.com/sites/MLA/search?q=${result["title"]}&limit=1`
-    ).then((result) => result.json());
-
-    const response = { author: { name: "Rudy", lastname: "Alvarado" } };
-
-    response.categories =
-      categories["filters"].length > 0
-        ? categories["filters"][0]["values"][0]["path_from_root"].map(
-            (category) => category.name
-          )
-        : [];
-
-    if (result["id"]) {
-      response.item = {
-        id: result["id"],
-        title: result["title"],
-        price: {
-          amount: result["price"].toString().split(".")[0],
-          currency: result["currency_id"],
-          decimals: result["price"].toString().split(".")[1],
-        },
-        picture: result["pictures"][0]["url"],
-        condition: result["condition"],
-        free_shipping: result["shipping"]["free_shipping"],
-        sold_quantity: result["sold_quantity"],
-        description: description["plain_text"],
-      };
-    }
-
-    res.status(200).json(response);
   } catch (error) {
     throw new Error(error.message);
   }
--- pages/api/items/index.js
+++ pages/api/items/index.js
@@ -1,34 +1,55 @@
+const { client } = require("../../../redis/index.js");
+
 export default async function handler(req, res) {
   try {
-    const results = await fetch(
-      `https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}&limit=4`
-    ).then((result) => result.json());
-
-    const response = { author: { name: "Rudy", lastname: "Alvarado" } };
-
-    response.categories =
-      results["filters"].length > 0
-        ? results["filters"][0]["values"][0]["path_from_root"].map(
-            (category) => category.name
-          )
-        : [];
-
-    response.items = results["results"].map((item) => {
-      return {
-        id: item["id"],
-        title: item["title"],
-        price: {
-          amount: item["price"].toString().split(".")[0],
-          currency: item["currency_id"],
-          decimals: item["price"].toString().split(".")[1],
-        },
-        picture: item["thumbnail"],
-        condition: item["condition"],
-        free_shipping: item["shipping"]["free_shipping"],
-      };
+    var results = await client.get(`q=${req.query.q}`, (err, results) => {
+      if (err) {
+        return null;
+      }
+      return results;
     });
 
-    res.status(200).json(response);
+    if (results) {
+      res.status(200).json(JSON.parse(results));
+      return;
+    } else {
+      results = await fetch(
+        `https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}&limit=4`
+      ).then((result) => result.json());
+
+      const response = { author: { name: "Rudy", lastname: "Alvarado" } };
+
+      response.categories =
+        results["filters"].length > 0
+          ? results["filters"][0]["values"][0]["path_from_root"].map(
+              (category) => category.name
+            )
+          : [];
+
+      response.items = results["results"].map((item) => {
+        return {
+          id: item["id"],
+          title: item["title"],
+          price: {
+            amount: item["price"].toString().split(".")[0],
+            currency: item["currency_id"],
+            decimals: item["price"].toString().split(".")[1],
+          },
+          picture: item["thumbnail"],
+          condition: item["condition"],
+          free_shipping: item["shipping"]["free_shipping"],
+        };
+      });
+
+      await client.set(
+        `q=${req.query.q}`,
+        JSON.stringify(response),
+        "EX",
+        parseInt(process.env.NEXT_PUBLIC_EXPIRES_AT)
+      );
+
+      res.status(200).json(response);
+    }
   } catch (error) {
     throw new Error(error.message);
   }
