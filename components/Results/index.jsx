ca952fd6a8416c7e8e0bfa751b66afdb6fb85f84
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-06-01 19:37:26 -0400

    feat(item url validation): item url validation
    Validation for "/items" && query different than "search", returns "Not found"

--- components/Results/index.jsx
+++ components/Results/index.jsx
@@ -1,36 +1,54 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from "next/router";
 import { itemSearch } from "../../redux/Item/ItemSearchSlice";
 import ResultsItem from "../ResultsItem";
 import Breadcrumbs from "../Breadcrumbs";
+import NotFound from "../NotFound";
 import { StyledBreadcrumbContainer, StyledResultsContainer } from "./styles";
 
 const Results = () => {
+  const [routeState, setRouteState] = useState("unset");
   const router = useRouter();
   const dispatch = useDispatch();
   const { loading, error, data } = useSelector((store) => store.itemSearch);
 
   useEffect(() => {
-    if (!loading && !error && !("items" in data)) {
-      const path = router.asPath.split("=");
-      dispatch(itemSearch(path[path.length - 1]));
+    if (
+      router.asPath === "/items" ||
+      (Object.keys(router.query).length === 1 && !("search" in router.query))
+    ) {
+      setRouteState("invalid");
+      return;
     }
-  }, [data, dispatch, error, loading, router.asPath]);
+
+    if (!loading && !error && !("items" in data)) {
+      let search = router.asPath.split("=");
+      search = search[search.length - 1];
+      dispatch(itemSearch(search));
+    }
+
+    setRouteState("valid");
+  }, [data, dispatch, error, loading, router.asPath, router.query]);
 
   return (
     <>
-      {data.categories && (
-        <StyledBreadcrumbContainer maxWidth="md">
-          <Breadcrumbs categories={data.categories} />
-        </StyledBreadcrumbContainer>
+      <>{routeState === "invalid" && <NotFound />}</>
+      {routeState === "valid" && (
+        <>
+          {data.categories && (
+            <StyledBreadcrumbContainer maxWidth="md">
+              <Breadcrumbs categories={data.categories} />
+            </StyledBreadcrumbContainer>
+          )}
+          <StyledResultsContainer maxWidth="md">
+            {JSON.stringify(data.items) &&
+              data.items.map((item) => {
+                return <ResultsItem key={item.title} item={item} />;
+              })}
+          </StyledResultsContainer>
+        </>
       )}
-      <StyledResultsContainer maxWidth="md">
-        {JSON.stringify(data.items) &&
-          data.items.map((item) => {
-            return <ResultsItem key={item.title} item={item} />;
-          })}
-      </StyledResultsContainer>
     </>
   );
 };
